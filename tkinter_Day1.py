0b1d690d31cfffb4b6b1830e33fd739c153301e3
Topology12 <duong120503mk@gmail.com>
2023-12-06 13:27:26 +0700

    Day1 tkinter

--- tkinter_Day1.py
+++ tkinter_Day1.py
@@ -1,1 +1,12 @@
 import tkinter as tk
+
+# New window graphics
+root = tk.Tk()
+
+# New label widget
+mylabel = tk.Label(root, text ="Hello World")
+# Shoving it onto the screen
+mylabel.pack()
+
+root.mainloop()
+
